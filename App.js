869cf211cd0cd44c97ebc8943a159e23fd7626b4
udosinachi <udosipat@gmail.com>
2020-04-09 17:04:15 +0100

    My first use of componentDidMount on React

---
+++ App.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import Conditional from './Conditional'
+
+class App extends React.Component{
+    constructor(){
+        super()
+        this.state = {
+            isLoading: true
+        }
+    }
+
+        componentDidMount() {
+            setTimeout(() => {
+                this.setState({
+                    isLoading: false
+                })
+            }, 1000)
+        }
+    
+    
+    render(){
+        return(
+            <div>
+                <Conditional isLoading={this.state.isLoading}/>
+            </div>
+        )
+    }
+}
+
+export default App
